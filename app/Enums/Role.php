9b6ea5afc6bc3f08bb0bff65211af3169c27dd22
PovilasKorop <povilask007@gmail.com>
2024-10-03 18:17:56 +0300

    No need label in Enum

--- app/Enums/Role.php
+++ app/Enums/Role.php
@@ -10,16 +10,4 @@ enum Role: string
     case ClinicAdmin = 'clinic-admin';
     case ClinicOwner = 'clinic-owner';
     case MasterAdmin = 'master-admin';
-
-    public function label(): string
-    {
-        return match ($this) {
-            self::Patient     => 'Patient',
-            self::Doctor      => 'Doctor',
-            self::Staff       => 'Staff',
-            self::ClinicAdmin => 'Clinic Admin',
-            self::ClinicOwner => 'Clinic Owner',
-            self::MasterAdmin => 'Master Admin',
-        };
-    }
 }
