79fbfdfdd6b3d9e0ab881cb299ad31dc74f6ac83
Nerijus <krekas@gmail.com>
2024-10-03 12:03:53 +0300

    Add more task tests

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -4,6 +4,7 @@
 use App\Models\User;
 use App\Models\Team;
 use App\Models\Task;
+use Illuminate\Support\Collection;
 use function Pest\Laravel\actingAs;
 
 it('allows clinic admin and staff to access create task page', function (User $user) {
@@ -48,7 +49,6 @@ it('allows clinic admin and staff to enter update page for any task in their tea
 it('does not allow administrator and manager to enter update page for other teams task', function (User $user) {
     $team = Team::factory()->create();
 
-    $taskUser = User::factory()->clinicAdmin()->create();
     $task = Task::factory()->create([
         'team_id' => $team->id,
     ]);
@@ -123,3 +123,44 @@ it('does not allow super admin and admin to delete task for other team', functio
     fn () => User::factory()->doctor()->create(),
     fn () => User::factory()->staff()->create(),
 ]);
+
+it('shows users with a role of doctor and staff as assignees', function () {
+    $doctor = User::factory()->doctor()->create();
+    $staff = User::factory()->staff()->create();
+
+    $clinicAdmin = User::factory()->clinicAdmin()->create();
+    $masterAdmin = User::factory()->masterAdmin()->create();
+    $patient = User::factory()->patient()->create();
+
+    actingAs($clinicAdmin)
+        ->get(route('tasks.create'))
+        ->assertViewHas('assignees', function (Collection $assignees) use ($doctor, $staff, $masterAdmin, $clinicAdmin, $patient): bool {
+            return $assignees->contains(fn (string $assignee) => $assignee === $doctor->name ||
+                    $assignee === $staff->name
+                ) && $assignees->doesntContain(fn (string $assignee) => $assignee === $masterAdmin->name
+                    || $assignee === $clinicAdmin->name
+                    || $assignee === $patient->name
+                );
+        });
+});
+
+it('shows users with a role of patient as patients', function () {
+    $doctor = User::factory()->doctor()->create();
+    $staff = User::factory()->staff()->create();
+
+    $clinicAdmin = User::factory()->clinicAdmin()->create();
+    $masterAdmin = User::factory()->masterAdmin()->create();
+    $patient = User::factory()->patient()->create();
+
+    actingAs($clinicAdmin)
+        ->get(route('tasks.create'))
+        ->assertViewHas('patients', function (Collection $patients) use ($patient, $doctor, $staff, $masterAdmin, $clinicAdmin): bool {
+            return $patients->contains(fn (string $value) => $value === $patient->name) &&
+                $patients->doesntContain(fn (string $value) =>
+                    $value === $doctor->name
+                    || $value === $staff->name
+                    || $value === $masterAdmin->name
+                    || $value === $clinicAdmin->name
+                );
+        });
+});
